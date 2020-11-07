44daaa8f4bc4abf9fda55800283648e06771cef1
Cloudy Young <cloudyyoung74@outlook.com>
2020-11-07 12:24:39 +0800

    Update 1rem size; Update illust styling; Update events properties

--- js/special.js
+++ js/special.js
@@ -78,7 +78,7 @@ $.events = [
         "name": [["柴田", "美月"], ["柴田", "美月"], ["Shibata", "Mizuki"]]
     },
     {
-        "key": "halloween8",
+        "key": "halloween-8",
         "month": 10,
         "day": 24,
         "charatype": "halloween",
@@ -86,7 +86,7 @@ $.events = [
         "text": ["ハッピーハロウィンウィーク！", "万圣节周快乐！", "Happy Halloween Week!"]
     },
     {
-        "key": "halloween7",
+        "key": "halloween-7",
         "month": 10,
         "day": 25,
         "charatype": "halloween",
@@ -94,7 +94,7 @@ $.events = [
         "text": ["ハッピーハロウィンウィーク！", "万圣节周快乐！", "Happy Halloween Week!"]
     },
     {
-        "key": "halloween6",
+        "key": "halloween-6",
         "month": 10,
         "day": 26,
         "charatype": "halloween",
@@ -102,7 +102,7 @@ $.events = [
         "text": ["ハッピーハロウィンウィーク！", "万圣节周快乐！", "Happy Halloween Week!"]
     },
     {
-        "key": "halloween5",
+        "key": "halloween-5",
         "month": 10,
         "day": 27,
         "charatype": "halloween",
@@ -110,7 +110,7 @@ $.events = [
         "text": ["ハッピーハロウィンウィーク！", "万圣节周快乐！", "Happy Halloween Week!"]
     },
     {
-        "key": "halloween4",
+        "key": "halloween-4",
         "month": 10,
         "day": 28,
         "charatype": "halloween",
@@ -118,7 +118,7 @@ $.events = [
         "text": ["ハッピーハロウィンウィーク！", "万圣节周快乐！", "Happy Halloween Week!"]
     },
     {
-        "key": "halloween3",
+        "key": "halloween-3",
         "month": 10,
         "day": 29,
         "charatype": "halloween",
@@ -126,7 +126,7 @@ $.events = [
         "text": ["ハッピーハロウィンウィーク！", "万圣节周快乐！", "Happy Halloween Week!"]
     },
     {
-        "key": "halloween2",
+        "key": "halloween-2",
         "month": 10,
         "day": 30,
         "charatype": "halloween",
@@ -134,7 +134,7 @@ $.events = [
         "text": ["ハッピーハロウィンウィーク！", "万圣节周快乐！", "Happy Halloween Week!"]
     },
     {
-        "key": "halloween1",
+        "key": "halloween-1",
         "month": 10,
         "day": 31,
         "showDate": true,
@@ -160,7 +160,7 @@ $.events = [
         "showDate": true,
         "charatype": "story",
         "charaface": "lina-kimono",
-        "text": ["初対面のレナ。", "初见莉娜", "First meet Lina"]
+        "text": ["初対面のレナ。", "初见莉娜", "First meet, Lina"]
     },
     {
         "key": "succession",
@@ -185,7 +185,7 @@ $.months = [
 $.events.makeCard = function (each) {
     let date = `${each.year ? each.year + "年" : ""}${each.month}月${each.day}日 // ${each.year ? each.year + "年" : ""}${each.month}月${each.day}日 // ${$.months[each.month - 1]} ${each.day}${each.year ? ", " + each.year : ""}`;
     return `
-    <div class="${each.type} ${each.key} ${Array.isArray(each.charaface) || !each.charaface ? "" : "has-side-chara"} card" style="display: none;">
+    <div class="${each.type} ${each.key} card ${each.hasSideChara && !each.hasIllust ? "has-side-chara" : ""}" style="display: none;">
         <span class= "chara ${each.charatype}" style="background-image: url('chara/${each.charatype}/${each.charaface}.png')" ></span>
         <span class="date ${each.showDate ? `` : `hide`} + " i18n>${date}</span>
         <span class="text" i18n>${each.text.join("//")}</span>
@@ -193,20 +193,38 @@ $.events.makeCard = function (each) {
 }
 
 $.events.makeEvent = function (each) {
-    return $.events.makeCard(each);
+    let html = ``;
+    each.type = "event";
+    if (Array.isArray(each.charaface)) {
+        each.hasIllust = true;
+        each.hasSideChara = false;
+        html += $.events.makeIllust(each);
+    } else if (each.charaface) {
+        each.hasIllust = true;
+        each.hasSideChara = true;
+        html += $.events.makeIllust(each);
+    } else {
+        each.hasIllust = false;
+        each.hasSideChara = false;
+    }
+    html += $.events.makeCard(each);
+    return html;
 }
 
 $.events.makeBirthday = function (each) {
     each.charatype = "charaface";
     each.showDate = true;
     each.charaface = each.key;
+    each.hasSideChara = true;
+    each.hasIllust = false;
     each.text = [`${each.name[0][1]}の誕生日おめでとう！`, `${each.name[1][1]}生日快乐！`, `Happy ${each.name[2][1]} Day!`];
-    return $.events.makeEvent(each);
+    return $.events.makeCard(each);
 }
 
 $.events.makeIllust = function (each) {
+    let charaface = Array.isArray(each.charaface) ? each.charaface : Array.from(each.charaface);
     html = `<div class="illust ${each.key}" style="display: none;">`;
-    each.charaface.forEach(chara => html += `<span class="charaface-full ${chara}" style="background-image: url('chara/story/${chara}.png');"></span>`);
+    charaface.forEach(chara => html += `<span class="charaface-full ${chara}" style="background-image: url('chara/story/${chara}.png');"></span>`);
     html += `</div>`;
     return html;
 }
@@ -214,26 +232,24 @@ $.events.makeIllust = function (each) {
 
 // Special Event
 $.events.forEach(each => {
-    let html = ``;
-    let date = `${each.year ? each.year + "年" : ""}${each.month}月${each.day}日 // ${each.year ? each.year + "年" : ""}${each.month}月${each.day}日 // ${$.months[each.month - 1]} ${each.day}${each.year ? ", " + each.year : ""}`;
     if (each.type == "birthday") {
-        html = $.events.makeBirthday(each);
+        $('.special.event').append($.events.makeBirthday(each));
     } else if (each.type == "illust") {
-        html = $.events.makeIllust(each);
+        $('.special.event').prepend($.events.makeIllust(each));
     } else {
-        each.type = "event";
-        html = $.events.makeEvent(each);
+        $('.special.event').append($.events.makeEvent(each));
     }
-    $('.special.event').append(html);
 });
 
-// Special
+$.events.on = [];
+
+// Current displaying events and their arrangement
 setInterval(function () {
     let today = new Date();
     let month = today.getMonth() + 1;
     let day = today.getDate();
 
-    month = 12, day = 31;
+    month = 1, day = 1;
 
     $.events.forEach(each => {
         let id = `.special.event .${each.type}.${each.key}`;
