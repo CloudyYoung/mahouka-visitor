1ffab6f9fdee59182904eb7133ae6ed2e13fe98b
Cloudy Young <cloudyyoung74@outlook.com>
2020-12-15 05:48:12 +0800

    Update

--- js/special.js
+++ js/special.js
@@ -39,118 +39,97 @@ $.events = [
         "charaface": ["tatsuya", "angie"],
         "show": false
     }, {
-        "key": "story-7",
-        "charatype": "uniform",
-        "charaface": ["mari", "mayumi", "kanon", "kei", "subaru", "eimi", "takeaki", "sayaka", "katsuto", "hanzou", "azusa", "suzune"],
-        "text": ["Happy Graduation", "Happy Graduation", "Happy Graduation"],
-        "show": true,
-    },
-    {
         "key": "miyuki",
         "type": "birthday",
         "month": 3,
         "day": 25,
         "name": [["司波", "深雪"], ["司波", "深雪"], ["Shiba", "Miyuki"]]
-    },
-    {
+    }, {
         "key": "tatsuya",
         "type": "birthday",
         "month": 4,
         "day": 24,
         "name": [["司波", "達也"], ["司波", "达也"], ["Shiba", "Tatsuya"]]
-    },
-    {
+    }, {
         "key": "mayumi",
         "type": "birthday",
         "month": 6,
         "day": 26,
         "name": [["七草", "真由美"], ["七草", "真由美"], ["Saegusa", "Mayumi"]]
-    },
-    {
+    }, {
         "key": "erika",
         "type": "birthday",
         "month": 8,
         "day": 28,
         "name": [["千葉", "エリカ"], ["千叶", "艾莉卡"], ["Chiba", "Erika"]]
-    },
-    {
+    }, {
         "key": "leo",
         "type": "birthday",
         "month": 8,
         "day": 1,
         "name": [["西城", "レオンハルト"], ["西城", "雷欧赫特"], ["Saijo", "Leonhard"]]
-    },
-    {
+    }, {
         "key": "mikihiko",
         "type": "birthday",
         "month": 9,
         "day": 10,
         "name": [["吉田", "幹比古"], ["吉田", "干比古"], ["Yoshida", "Mikihiko"]]
-    },
-    {
+    }, {
         "key": "mizuki",
         "type": "birthday",
         "month": 9,
         "day": 25,
         "name": [["柴田", "美月"], ["柴田", "美月"], ["Shibata", "Mizuki"]]
-    },
-    {
+    }, {
         "key": "halloween-8",
         "month": 10,
         "day": 24,
         "charatype": "halloween",
         "charaface": "azusa",
         "text": ["ハッピーハロウィンウィーク！", "万圣周快乐！", "Happy Halloween Week!"]
-    },
-    {
+    }, {
         "key": "halloween-7",
         "month": 10,
         "day": 25,
         "charatype": "halloween",
         "charaface": "honoka",
         "text": ["ハッピーハロウィンウィーク！", "万圣周快乐！", "Happy Halloween Week!"]
-    },
-    {
+    }, {
         "key": "halloween-6",
         "month": 10,
         "day": 26,
         "charatype": "halloween",
         "charaface": "mari",
         "text": ["ハッピーハロウィンウィーク！", "万圣周快乐！", "Happy Halloween Week!"]
-    },
-    {
+    }, {
         "key": "halloween-5",
         "month": 10,
         "day": 27,
         "charatype": "halloween",
         "charaface": "shizuku",
         "text": ["ハッピーハロウィンウィーク！", "万圣周快乐！", "Happy Halloween Week!"]
-    },
-    {
+    }, {
         "key": "halloween-4",
         "month": 10,
         "day": 28,
         "charatype": "halloween",
         "charaface": "erika",
         "text": ["ハッピーハロウィンウィーク！", "万圣周快乐！", "Happy Halloween Week!"]
-    },
-    {
+    }, {
         "key": "halloween-3",
         "month": 10,
         "day": 29,
         "charatype": "halloween",
         "charaface": "lina",
         "text": ["ハッピーハロウィンウィーク！", "万圣周快乐！", "Happy Halloween Week!"]
-    },
-    {
+    }, {
         "key": "halloween-2",
         "month": 10,
         "day": 30,
         "charatype": "halloween",
         "charaface": "miyuki",
         "text": ["ハッピーハロウィンウィーク！", "万圣周快乐！", "Happy Halloween Week!"]
-    },
-    {
+    }, {
         "key": "halloween-1",
         "year": 2095,
         "month": 10,
@@ -159,8 +138,7 @@ $.events = [
         "charatype": "halloween",
         "charaface": "tatsuya",
         "text": ["灼熱のハロウィン", "烧焦的万圣节", "Scorched Halloween"]
-    },
-    {
+    }, {
         "key": "enrollment-day",
         "year": 2095,
         "month": 4,
@@ -170,8 +148,7 @@ $.events = [
         "charatype": "charaface",
         "charaface": "miyuki",
         "text": ["シブリングスクールの日", "兄妹入学日", "Enrollment Day"]
-    },
-    {
+    }, {
         "key": "first-meet-lina",
         "year": 2096,
         "month": 1,
@@ -180,8 +157,7 @@ $.events = [
         "charatype": "story",
         "charaface": "lina-kimono",
         "text": ["初対面のレナ。", "初见莉娜", "First meet, Lina"]
-    },
-    {
+    }, {
         "key": "succession",
         "year": 2096,
         "month": 12,
@@ -190,6 +166,36 @@ $.events = [
         "charatype": "story",
         "charaface": ["miyuki", "tatsuya"],
         "text": ["相続。婚約。", "继承。婚约。", "Succession. Engagement."]
+    }, {
+        "key": "graduation-class-2097-1",
+        "charatype": "uniform",
+        "charaface": ["katsuto", "hanzou"],
+        "text": ["2 0 9 7 年 卒業おめでとう！", "第 2097 届 恭喜毕业！", "Class 2097 Congratulations on Graduation!"],
+        "show": true,
+    }, {
+        "key": "graduation-class-2097-2",
+        "charatype": "uniform",
+        "charaface": ["mari", "mayumi"],
+        "text": ["2 0 9 7 年 卒業おめでとう！", "第 2097 届 恭喜毕业！", "Class 2097 Congratulations on Graduation!"],
+        "show": true,
+    }, {
+        "key": "graduation-class-2097-3",
+        "charatype": "uniform",
+        "charaface": ["azusa", "suzune"],
+        "text": ["2 0 9 7 年 卒業おめでとう！", "第 2097 届 恭喜毕业！", "Class 2097 Congratulations on Graduation!"],
+        "show": true,
+    }, {
+        "key": "graduation-class-2097-4",
+        "charatype": "uniform",
+        "charaface": ["kanon", "kei"],
+        "text": ["2 0 9 7 年 卒業おめでとう！", "第 2097 届 恭喜毕业！", "Class 2097 Congratulations on Graduation!"],
+        "show": true,
+    }, {
+        "key": "graduation-class-2097-5",
+        "charatype": "uniform",
+        "charaface": ["takeaki", "sayaka"],
+        "text": ["2 0 9 7 年 卒業おめでとう！", "第 2097 届 恭喜毕业！", "Class 2097 Congratulations on Graduation!"],
+        "show": true,
     }
 ];
 
@@ -249,7 +255,7 @@ $.events.makeIllust = function (each) {
     let charaface = [];
     if (Array.isArray(each.charaface)) charaface = each.charaface;
     else if (each.charaface) charaface = [each.charaface];
-    html = `<div class="illust ${each.key}">`;
+    html = `<div class="illust ${each.key} ${each.charatype}">`;
     charaface.forEach(chara => html += `<span class="charaface ${chara}" style="background-image: url('chara/${each.charatype}/${chara}.png');"></span>`);
     html += `</div>`;
     return html;
